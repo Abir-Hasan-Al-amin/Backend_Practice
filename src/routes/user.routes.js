00bf5c86e436865c0d9ae18a3255c2be1dee1fcc
Abir Hasan Al-amin <hasan.alamin.abir+gitHUB@gmail.com>
2024-09-15 13:50:17 +0600

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { getWatchHistory, loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
@@ -30,9 +30,10 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/password-change").post(verifyJWT,changeCurrentPassword);
-router.route("/current-user").post(verifyJWT,getCurrentUser);
-router.route("/update-details").post(verifyJWT,updateAccountDetails);
-router.route("/update-avatar").post(verifyJWT,updateUserAvatar);
-router.route("/update-coverImage").post(verifyJWT,updateUserCoverImage);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-details").patch(verifyJWT,updateAccountDetails);
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar);
+router.route("/update-coverImage").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage);
 router.route("/get-user-channel-profile/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/get-watch-history").get(verifyJWT,getWatchHistory);
 export default router;
